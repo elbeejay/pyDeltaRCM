bca0635269e86e3eeb05aec55d3e45623296bb6c
elbeejay <jayaram.hariharan@utexas.edu>
2020-07-09 12:29:18 +0000

    Deploying to gh-pages from  @ 7270b70b7fd2fc3ab89e5a8713ed478979da30ea 🚀

---
+++ pyplots/10min/model_run_visual.py
@@ -0,0 +1,20 @@
+import sys
+import os
+# sys.path.append(os.path.realpath(os.getcwd() + "/../../../../"))
+
+import matplotlib.pyplot as plt
+
+import pyDeltaRCM
+
+
+delta = pyDeltaRCM.DeltaModel()
+
+for _t in range(0, 2):
+    delta.update()
+
+delta.finalize()
+
+
+fig, ax = plt.subplots()
+ax.imshow(delta.bed_elevation, vmax=-4)
+plt.show()
